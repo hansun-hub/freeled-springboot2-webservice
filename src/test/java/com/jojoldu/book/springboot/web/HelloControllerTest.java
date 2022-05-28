1a466e4f0e25dc1b53c3ca69aaad1730d3bb9ed2
hansun-hub <hansun33@naver.com>
2022-05-28 21:55:00 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹서비스 3장 ~3-3

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -24,9 +24,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
