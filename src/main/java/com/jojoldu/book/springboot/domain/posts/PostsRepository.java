98730fe81b3c23c7a9656f5a987fe5e50f04990d
hansun-hub <hansun33@naver.com>
2022-07-24 11:08:08 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹서비스 7장-2

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,6 @@
 package com.jojoldu.book.springboot.domain.posts;
 
-import javafx.geometry.Pos;
+//import javafx.geometry.Pos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
